fc1fc7f62633b884f173b078713ce8a76cdba542
kareza <kareza@qq.com>
2020-09-26 10:18:57 +0800

    添加redis连接和关闭redis服务测试代码

--- src/test/java/cn/kareza/redisdemo/RedisDemoApplicationTests.java
+++ src/test/java/cn/kareza/redisdemo/RedisDemoApplicationTests.java
@@ -2,12 +2,34 @@ package cn.kareza.redisdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import redis.clients.jedis.Jedis;
 
 @SpringBootTest
 class RedisDemoApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void redisConnect() {
+        // 实例化连接对象
+        Jedis jedis = new Jedis("www.kareza.cn", 6379);
+        jedis.auth("kareza");
+        System.out.println("连接www.kareza.cn的redis成功");
+        // 查看服务是否运行
+        System.out.println("运行状态，PING指令返回 " + jedis.ping());
+    }
+
+    @Test
+    void redisShutdown() {
+        // 实例化连接对象
+        Jedis jedis = new Jedis("www.kareza.cn", 6379);
+        jedis.auth("kareza");
+        System.out.println("连接www.kareza.cn的redis成功");
+        // 查看服务是否运行
+        System.out.println("关闭redis服务");
+        jedis.shutdown();
     }
 
 }
